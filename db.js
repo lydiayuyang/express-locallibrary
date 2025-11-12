88416f1b51c4032b09f29c9b9594c38690cbf569
lydiayuyang <lydia_yuyang@outlook.com>
2025-11-12 18:16:21 +0800

    Update db.js to support local and Atlas MongoDB

--- db.js
+++ db.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URI || "mongodb://127.0.0.1/my_database";
+
+const atlasUser = "lydia_yuyang@outlook.com";
+const atlasPassword = "yy821178...";
+const atlasDB="yuyang";
+const atlasURI= "mongodb+srv://yuyang:yy821178...@cluster0.2ytpmzk.mongodb.net/?appName=Cluster0";
+
+const localURI="mongodb://127.0.0.1:27017/my_database";
+const mongoDB=process.env.NODE_ENV==="production" ? atlasURI:localURI;
+
 mongoose.connect(mongoDB,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 });
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on("error",console.error.bind(console,"MongoDB error:"));
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console, "MongoDB connection error:"));
+db.once("open",()=>{
+    console.log("MongoDB connected successfully! Current URI:", mongoDB);
+});
+
 
 module.exports =db;
