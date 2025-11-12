db2cceefbac5a4c9b4642cde8bac57bac2c7f293
lydiayuyang <lydia_yuyang@outlook.com>
2025-11-12 18:55:01 +0800

    Update db.js and gitignore to account

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
 
-const atlasUser = "lydia_yuyang@outlook.com";
-const atlasPassword = "yy821178...";
-const atlasDB="yuyang";
-const atlasURI= "mongodb+srv://yuyang:yy821178...@cluster0.2ytpmzk.mongodb.net/?appName=Cluster0";
-
 const localURI="mongodb://127.0.0.1:27017/my_database";
+const atlasURI= process.env.MONGODB_URI;
 const mongoDB=process.env.NODE_ENV==="production" ? atlasURI:localURI;
 
-mongoose.connect(mongoDB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-});
+mongoose.connect(mongoDB);
 
 const db=mongoose.connection;
 db.on("error",console.error.bind(console, "MongoDB connection error:"));
