f87502a9a25132f04297afb4de8b9c227af9eb32
lydiayuyang <lydia_yuyang@outlook.com>
2025-11-12 16:33:04 +0800

    added files and changes required for deployment to heroku

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
-const mongoDB = "mongodb://127.0.0.1/my_database";
-mongoose.connect(mongoDB);
+const mongoDB = process.env.MONGODB_URI || "mongodb://127.0.0.1/my_database";
+mongoose.connect(mongoDB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"MongoDB error:"));
+
+module.exports =db;
